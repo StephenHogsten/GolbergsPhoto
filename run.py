fd2156ef78ad0d3f284457b87eb4d8f22b963f69
Stephen Hogsten <stephenhogsten@gmail.com>
2017-07-12 22:19:06 -0400

    updating run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@
 from app import app
 import os.environ
 port = int(os.environ.get('PORT', 5000))
-debug = False if os.environ.get('ENVIRONMENT') == production else True
+debug = False if os.environ.get('ENVIRONMENT') == 'production' else True
 app.run(port=port, debug=debug)
