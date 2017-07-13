01b7076096b8c7f82aed86d164890de4aae841e7
Stephen Hogsten <stephenhogsten@gmail.com>
2017-07-12 22:40:10 -0400

    added host

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import app
 from os import environ
 port = int(environ.get('PORT', 5000))
 debug = False if environ.get('ENVIRONMENT') == 'production' else True
-app.run(port=port, debug=debug)
+app.run(host='0.0.0.0', port=port, debug=debug)
