0e85768a95eed2fe0c478336b9288d19129ab29c
Stephen Hogsten <stephenhogsten@gmail.com>
2017-07-12 22:18:25 -0400

    updating py to have production settings

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 #!flask/bin/python
 from app import app
-app.run(debug=True)
+import os.environ
+port = int(os.environ.get('PORT', 5000))
+debug = False if os.environ.get('ENVIRONMENT') == production else True
+app.run(port=port, debug=debug)
